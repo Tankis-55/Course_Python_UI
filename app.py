5144bef83bf2069ef10b1d6b0533aa840ebf0d58
Tankis-55 <129220867+Tankis-55@users.noreply.github.com>
2024-11-10 13:29:10 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import requests
+
+
+API_URL = 'http://127.0.0.1:5000/items'
+
+### Service fetch_items
+def fetch_items():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        return response.json()
+    st.error(f'Error fetching items : {response.status_code}')
+    return []
+
+### Service fetch_item by id
+def fetch_item():
+    response = requests.get(f'{API_URL}/{item_id}')
+    if response.status_code == 200:
+        return response.json()
+    st.error(f'Error fetching item by ID : {response.status_code}')
+    return None
+
+###Frontend
+st.title('Custom application with backend')
+
+###Fetch Items
+st.subheader('Fetch Items')
+if st.button('Fetch all Items'):
+    items = fetch_items()
+    st.write(items)
+
+###Fetch Items by id
+st.subheader('Fetch Items by ID')
+item_id = st.number_input('Item ID', min_value=1)
+if st.button('Fetch  Item'):
+    item = fetch_item()
+    st.write(item)
